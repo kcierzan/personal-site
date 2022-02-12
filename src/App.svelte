2f0cb7120adf3f165d457fef954054b165f97214
Kyle Cierzan <kcierzan@gmail.com>
2022-02-12 01:18:32 -0500

    Add landing page

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+    import Navbar from "./Navbar.svelte";
+    export let name;
+
+    let visible = {
+        about: false,
+        resume: false,
+        projects: false,
+        contact: false
+    }
+    function handleNavigate(event) {
+        visible = event.detail;
+    }
+</script>
+
+<Navbar on:navigate={handleNavigate} visible={visible}/>
+<main>
+    <div>
+        <h1>Kyle <span>Cierzan</span></h1>
+        <h2>Software Engineer</h2>
+    </div>
+</main>
+
+<style>
+
+    @font-face {
+        font-family: "Iosevka";
+        font-weight: normal;
+        src: url('iosevka-ss14-medium.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: "Iosevka";
+        font-weight: bold;
+        src: url('iosevka-ss14-bold.woff2') format('woff2');
+    }
+
+    @font-face {
+        font-family: "Iosevka Light";
+        src: url('iosevka-ss14-light.woff2') format('woff2');
+    }
+
+    main {
+        text-align: center;
+        font-family: "Iosevka", monospace;
+    }
+
+    h1 {
+        font-size: 5em;
+        margin-top: 20%;
+        font-weight: normal;
+        font-family: "Iosevka Light", monospace;
+        white-space: nowrap;
+        margin-bottom: 0;
+    }
+
+    h2 {
+        position: relative;
+        font-family: "Iosevka Light", monospace;
+        font-size: 1.2em;
+        margin-top: 0;
+        top: 0;
+        left: 8em;
+    }
+
+    span {
+        font-family: "Iosevka", monospace;
+        font-weight: bold;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
---
+++ src/Navbar.svelte
@@ -0,0 +1,64 @@
+<script>
+    import {createEventDispatcher} from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    export let visible;
+
+    function navigate(page) {
+        const nulled = {
+            about: false,
+            projects: false,
+            resume: false,
+            contact: false
+        }
+        dispatch('navigate', {
+            ...nulled, [page]: true
+        })
+    }
+
+</script>
+
+<nav>
+    <a href="" on:click|preventDefault={() => navigate("about")} class:selected="{visible.about}">
+        About
+    </a>
+    <a href="" on:click|preventDefault={() => navigate("projects")} class:selected="{visible.projects}">
+        Projects
+    </a>
+    <a href="" on:click|preventDefault={() => navigate("resume")} class:selected="{visible.resume}">
+        Résumé
+    </a>
+    <a href="" on:click|preventDefault={() => navigate("contact")} class:selected="{visible.contact}">
+        Contact
+    </a>
+</nav>
+
+<style>
+    nav {
+        font-family: "Iosevka", monospace;
+        position: absolute;
+        right: 5%;
+        top: 5%;
+    }
+
+    a {
+        color: #000000;
+        display: inline-block;
+        font-size: 1.5em;
+        margin: 0 .2em 0 .2em;
+        text-decoration: underline 3px;
+        text-decoration-color: #ffffff;
+        transition: text-decoration-color 0.75s;
+    }
+
+    a.selected {
+        text-decoration: underline 3px;
+        text-decoration-color: #000;
+    }
+
+    a:hover {
+        text-decoration-color: indianred;
+    }
+
+</style>
